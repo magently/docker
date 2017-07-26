292c40dbf7afe7fa1e822b04e5253e98b8ea6922
Paweł Tatarczuk <pawel.tatarczuk@magently.com>
2017-07-26 13:16:03 +0200

    Use fixed tag for images
    This is meant to resolve issues with building ontop wrong images.
    Now we can include fixed tags in child Dockerfiles.
    
    refs #57609
    
    Change-Id: I31eab01d303b99cbbc21697e13255767d82f9af9

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
 
-# Set tag
-TAG="${DOCKER_TAG:=magently}"
-
 # Set registry
 REGISTRY="${DOCKER_REGISTRY:=registry.magently.com}"
 
 # Tag images
-docker tag $TAG/base:php5 $REGISTRY/$TAG/base:php5
-docker tag $TAG/base:php7 $REGISTRY/$TAG/base:php7
+docker tag magently/base:php5 $REGISTRY/magently/base:php5
+docker tag magently/base:php7 $REGISTRY/magently/base:php7
 
 # Publish images
-docker push $REGISTRY/$TAG/base:php5
-docker push $REGISTRY/$TAG/base:php7
+docker push $REGISTRY/magently/base:php5
+docker push $REGISTRY/magently/base:php7

46a6ddd624e9675f4783c467e41697b7ebd7e918
Paweł Tatarczuk <pawel.tatarczuk@magently.com>
2017-07-26 13:18:44 +0200

    Migrate from custom docker registry to docker hub with travis deployment
    refs #57874
    
    Change-Id: I9a3f6e411873fe23afabeeacb83fceaa1f49a2bb

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-# Set registry
-REGISTRY="${DOCKER_REGISTRY:=registry.magently.com}"
-
-# Tag images
-docker tag magently/base:php5 $REGISTRY/magently/base:php5
-docker tag magently/base:php7 $REGISTRY/magently/base:php7
-
 # Publish images
-docker push $REGISTRY/magently/base:php5
-docker push $REGISTRY/magently/base:php7
+docker push magently/base:php5
+docker push magently/base:php7

f3ac2937204385a09afdc90e4cd822ca4b6716a7
Paweł Tatarczuk <pawel.tatarczuk@magently.com>
2017-11-06 12:49:23 +0100

    Bugfix: Enforce symlinking local packages in magento2 images
    refs #58676
    
    Change-Id: Ia9f1fefae045cb9f154a2ad5b9b7fd36d5370da0

--- docker/magento2/conf/provision/entrypoint.d/98-install-module.sh
+++ docker/magento2/conf/provision/entrypoint.d/98-install-module.sh
@@ -3,13 +3,23 @@ set -e
 umask 002
 
 # Discover packages
-packages="$(find "$PACKAGES_PATH" -maxdepth 3 -type f -name composer.json \
+packages=$(find "$PACKAGES_PATH" -maxdepth 3 -type f -name composer.json \
     -exec grep -Po '"name":\s*"[^"/]+/[^"/]+"' {} \; \
-    | cut -d: -f2 | tr -d ' "' | xargs)"
+    | cut -d: -f2 | tr -d ' "' | awk 'NF{print $0 ":@dev"}' | xargs)
 
 # Require packages
-gosu "application" composer config -d "$MAGENTO_PATH" repositories.1 "{\"type\": \"path\", \"url\": \"$PACKAGES_PATH/*/*\"}" 2>/dev/null
+gosu "application" composer config -d "$MAGENTO_PATH" repositories.1 "{\"type\": \"path\", \"url\": \"$PACKAGES_PATH/*/*\", \"options\": {\"symlink\": true}}" 2>/dev/null
 [ -n "$packages" ] && gosu "application" composer require -d "$MAGENTO_PATH" $packages
+
+# Check if all packages have been symlinked
+for package in $packages; do
+    name="$(echo $package | cut -d: -f1)"
+
+    if [ ! -L "$MAGENTO_PATH/vendor/$name" ]; then
+        echo "$name package has not been symlinked... terminating"
+        exit 2
+    fi
+done
 
 # Wait for database
 dockerize -timeout 30s -wait tcp://$MYSQL_HOST:3306

56201b6682ce8e471c8991f7f7f45ba6e6751650
Adrian Szulc <adrian.szulc@magently.com>
2018-03-16 16:42:00 +0100

    Change the order of composer repositiories in Magento 2 install-module entrypoint
    Change-Id: Ic5bfcaccdda31f1cbb48508625dc80fecfb5d0b5

--- docker/magento2/conf/provision/entrypoint.d/80-install-module.sh
+++ docker/magento2/conf/provision/entrypoint.d/80-install-module.sh
@@ -7,8 +7,12 @@ packages=$(find "$PACKAGES_PATH" -maxdepth 3 -type f -name composer.json \
     -exec grep -Po '"name":\s*"[^"/]+/[^"/]+"' {} \; \
     | cut -d: -f2 | tr -d ' "' | awk 'NF{print $0 ":@dev"}' | xargs)
 
+# Manage repositiories
+# Workaround: first, we want to use packages from $PACKAGES_PATH, and then from Magento repository. The order of repositories is important.
+gosu "application" composer config -d "$MAGENTO_PATH" repositories.0 "{\"type\": \"path\", \"url\": \"$PACKAGES_PATH/*/*\", \"options\": {\"symlink\": true}}" 2>/dev/null
+gosu "application" composer config -d "$MAGENTO_PATH" repositories.1 "{\"type\": \"composer\", \"url\": \"https://repo.magento.com/\"}" 2>/dev/null
+
 # Require packages
-gosu "application" composer config -d "$MAGENTO_PATH" repositories.1 "{\"type\": \"path\", \"url\": \"$PACKAGES_PATH/*/*\", \"options\": {\"symlink\": true}}" 2>/dev/null
 [ -n "$packages" ] && gosu "application" composer require -d "$MAGENTO_PATH" $packages
 
 # Check if all packages have been symlinked

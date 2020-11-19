ed2cfdf4c48a08b1ce58d71be13cfeb46c37431c
d <d>
2020-11-19 16:44:34 +0100

    [php-test] Lock Composer version to 1.10.17
    Change-Id: If34ca684bb0acb273b50a6c0270da5cba9da9485

--- docker/php-test/composer.sh
+++ docker/php-test/composer.sh
@@ -11,7 +11,7 @@ then
     exit 1
 fi
 
-php composer-setup.php --quiet --install-dir=/usr/local/bin --filename composer
+php composer-setup.php --quiet --install-dir=/usr/local/bin --filename composer --version 1.10.17
 RESULT=$?
 rm composer-setup.php
 exit $RESULT
